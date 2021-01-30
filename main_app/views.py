c116473d45f46ba794472a163effc4f5a5a90633
YuUUTta <dsduoa31@gmail.com>
2021-01-30 23:23:24 +0900

    progress

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Record,Major
 from .forms import RecordForm
+from django_pandas.io import read_frame
+import pandas as pd
+import numpy as np
 
 # Create your views here.
 def home(request):
@@ -32,9 +35,54 @@ def search(request):
 
 
 def list(request):
+    result = pd.DataFrame()
     try:
         param = request.GET['pref']
+        datas = Record.objects.all().filter(place=param)
     except:
         param = request.GET['major']
-    datas = Record.objects.all().filter(place=param)
-    return render(request, 'list.html', {'datas':datas})
+        datas = Record.objects.all().filter(major=param)
+    df_o = read_frame(datas)
+    df = df_o[["hospital_name","place",'review','major']]
+    dx = df[["hospital_name","place",'review','major']]
+    #病院名の重複を削除 df
+    for i in range(len(df)):
+        if df.duplicated(subset='hospital_name')[i] == True:
+            df = df.drop(index=i)
+    df = df.reset_index(drop=True)
+    for f in range(len(df)):
+        dz = df_o
+        dz = dz[["hospital_name","place",'review','major']]
+        #一つの病院に限定 dz
+        for j in range(len(dz)):
+            if df.loc[f,'hospital_name'] != dz.loc[j,'hospital_name']:
+                dz = dz.drop(index=j)
+        dz = dz.reset_index(drop=True)
+        #重複のある専攻を削除
+        for i in range(len(dz)):
+            if dz.duplicated(subset='major')[i] == True:
+                dz = dz.drop(index=i)
+        dz = dz.reset_index(drop=True)
+        #同じ病院名において、データのある専攻を記載
+        for x in range(len(dx)):
+            for z in range(len(dz)):
+                if dx.loc[x,'hospital_name'] == dz.loc[z,'hospital_name']:
+                    dx.loc[x,'major_'+str(z)] = dz.loc[z,'major']
+        result = dx
+        #病院名の重複を削除 dx
+        for i in range(len(result)):
+            if result.duplicated(subset='hospital_name')[i] == True:
+                result = result.drop(index=i)
+        #スコアの導出
+        for f in range(len(result)):
+            score = np.zeros(0)
+            for x in range(len(dx)):
+                if result.loc[f,'hospital_name'] == dx.loc[x,'hospital_name']:
+                    score = np.append(score, dx.loc[x,'review'])
+            result.loc[f,'review'] = round(score.mean(),1)
+            result.loc[f,'count'] = str(len(score))
+    result = result.fillna(0)
+    #print(result)
+
+
+    return render(request, 'list.html', {'result':result})
