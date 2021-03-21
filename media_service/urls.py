7918ae4537a35199a2c2f5a5702ccb1cbc8bbb93
Heroku1234 <ive.no.int.to.sur@gmail.com>
2021-03-21 20:58:15 +0900

    media progress

--- media_service/urls.py
+++ media_service/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 app_name="media_service"
 
 urlpatterns = [
-    path('',views.main,name='main'),
+    path('',views.main,name='main_url'),
     path('making_now',views.making_now,name='making_now_url'),
 ]
--- media_service/views.py
+++ media_service/views.py
@@ -1,10 +1,12 @@
 # Public Django
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 # Public Python
 import requests
 #Private Django
 
 # Create your views here.
+@permission_required('admin.can_add_log_entry')
 def main(request):
     return render(request,'media_service/main.html')
 
