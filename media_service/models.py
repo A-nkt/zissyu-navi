ef2abc17e6e4e26d56d474ffb7cff797176ee2ee
YuUuTta <dsduoa31@gmail.com>
2021-03-29 07:05:18 +0000

    progress

--- media_service/models.py
+++ media_service/models.py
@@ -4,15 +4,15 @@ from django.utils import timezone
 from django.utils.translation import gettext as _
 # Create your models here.
 #image title RichTextField date discription
-class Article(models.Model):
+class Blog(models.Model):
     class Meta:
-        db_table = 'Article'
+        db_table = 'Blog'
         verbose_name = _('メディア記事')
         verbose_name_plural = _('メディア記事')
 
     title = models.CharField(verbose_name='タイトル',max_length=40,blank=True,null=True)
     image = models.ImageField(upload_to='media/',blank=True,null=True)
-    #body = RichTextField(blank=True,null=True)
+    body = RichTextField(blank=True,null=True)
     date = models.DateField(verbose_name='更新日',blank=True,null=True,default=timezone.now)
     discription = models.TextField(blank=True,null=True,max_length=50)
 
